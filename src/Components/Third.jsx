238ee8e5a0cb8e21ba1780550bc48f77cc2ddf24
Rehan <rehanalire52@gmail.com>
2024-02-01 19:18:11 +0530

    Update Third Page

--- src/Components/Third.jsx
+++ src/Components/Third.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 function Third() {
     return (
-        <div className=' lg:h-[100vh] h-[150vh] pl-40 flex flex-col items-start justify-center w-full bg-cover bg-no-repeat bg-[url(https://web-images.credcdn.in/_next/assets/images/home-page/money-matters-bg.jpg)]'>
-            <h1 className='text-white font-bold text-[60px]'>we take your money <br />
+        <div className=' lg:h-[100vh] h-[150vh] pl-8 lg:pl-40 flex flex-col bg-img3 items-start justify-center w-full'>
+            <div className='w-[70vw]'>
+            <h1 className='text-white font-bold text-5xl lg:text-7xl'>we take your money <br />
                 matters seriously.</h1>
-                <p className='text-xl font-bold text-white'>so that you don’t have to.</p>
-                <p className='text-white  mt-16 text-[14px] font-semibold'>never miss a due date with reminders to help you pay your bills on time, <br /> instant settlements mean you never wait for your payments to go through <br /> and statement analysis lets you know where your money goes, always.</p>
-                <button className='px-10 hover:bg-transparent border-2 hover:text-white py-2 mt-8 bg-white text-black font-semibold text-lg rounded-full'>Experience the upgrade</button>
+            </div>
+                <p className='lg:text-xl text-sm font-bold mt-4 lg:mt-2 text-white'>so that you don’t have to.</p>
+                <div className='w-[75vw] mt-14 lg:mt-0 '>
+                <p className='text-white mt-10 text-xs lg:text-sm font-normal lg:font-semibold'>never miss a due date with reminders to help you pay your bills on time, <br /> instant settlements mean you never wait for your payments to go through <br /> and statement analysis lets you know where your money goes, always.</p>
+                </div>
+                <button className='lg:px-10 px-7 hover:bg-transparent border-2 hover:text-white py-2 mt-8 bg-white text-black font-medium lg:font-semibold text-base lg:text-lg rounded-full'>Experience the upgrade</button>
         </div>
     )
 }
