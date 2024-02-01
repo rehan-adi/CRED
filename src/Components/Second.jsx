e4dfd77eff7844925cc9401d9b2488eec01bbccc
Rehan <rehanalire52@gmail.com>
2024-02-01 12:14:40 +0530

    Update Second Page

--- src/Components/Second.jsx
+++ src/Components/Second.jsx
@@ -3,12 +3,20 @@ import React from 'react'
 function Second() {
   return (
     <>
-    <div className='h-[100vh] flex items-start pl-40 flex-col justify-center bg-[url(https://web-images.credcdn.in/_next/assets/images/home-page/deserve-more-bg.jpg)] bg-cover bg-no-repeat w-full '>
-      <h1 className='text-white font-bold text-[60px]'>feel special <br />
+    <div className='lg:h-[100vh] h-[130vh] flex items-start lg:pl-40 flex-col justify-center bg-[url(https://web-images.credcdn.in/_next/assets/images/home-page/deserve-more-bg.jpg)] bg-cover bg-no-repeat w-full '>
+      <div className='w-[60vw] pl-8 lg:pl-0 '>
+      <h1 className='text-white font-bold text-5xl lg:text-7xl'>feel special <br />
         more often.</h1>
-        <p className='text-xl font-bold text-white'>exclusive rewards for paying your bills</p>
-        <p className='text-white mt-16 text-[14px] font-semibold'>every time you pay your credit card bills on CRED, you receive CRED coins. <br /> you can use these to win exclusive rewards or get special access to curated <br /> products and experiences. on CRED, good begets good.</p>
-        <button className='px-10 hover:bg-transparent border-2 hover:text-white py-2 mt-8 bg-white text-black font-semibold text-lg rounded-full'>Explor Reward</button>
+      </div>
+        <div className='w-[85vw] mt-5 lg:mt-4 pl-8 lg:pl-0'>
+        <p className='lg:text-xl text-base font-bold text-white'>exclusive rewards for paying your bills</p>
+        </div>
+        <div className='w-[80vw] lg:w-[35vw] pl-8 mt-16 lg:mt-0 lg:pl-0 '>
+        <p className='text-white mt-16 lg:text-sm text-xs font-medium lg:font-semibold'>every time you pay your credit card bills on CRED, you receive CRED coins. you can use these to win exclusive rewards or get special access to curated products and experiences. on CRED, good begets good.</p>
+        </div>
+        <div className='pl-8 lg:pl-0'>
+        <button className='lg:px-10 px-7 hover:bg-transparent border-2 hover:text-white py-2 mt-8 bg-white text-black font-semibold text-base lg:text-lg rounded-full'>Explor Reward</button>
+        </div>
     </div>
     <div className='bg-white flex flex-col justify-center items-center w-full h-[35vh]'>
            <div>
