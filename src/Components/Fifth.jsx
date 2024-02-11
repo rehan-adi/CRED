fd37f243134a4d44e15a5e6ebea6570efd827d85
Rehan <rehanalire52@gmail.com>
2024-02-11 21:52:16 +0530

    Again small update on Fifth page

--- src/Components/Fifth.jsx
+++ src/Components/Fifth.jsx
@@ -6,16 +6,16 @@ function Fifth() {
     <div className='Bokachoda'>
 
     </div>
-    <div className=' lg:h-[100vh] h-[120vh] lg:pl-40 pl-8 flex flex-col items-start justify-center w-full bg-cover bg-no-repeat bg-[url(https://web-images.credcdn.in/_next/assets/images/home-page/security-bg.jpg)]'>
+    <div className=' lg:h-[100vh] md:h-[80vh] h-[120vh] lg:pl-40 pl-8 flex flex-col items-start justify-center w-full bg-cover bg-no-repeat bg-[url(https://web-images.credcdn.in/_next/assets/images/home-page/security-bg.jpg)]'>
            <div className='w-[70vw]'>
-           <h1 className='text-white font-bold text-5xl lg:text-7xl'>security first. <br />
+           <h1 className='text-white font-bold text-5xl md:text-6xl lg:text-7xl'>security first. <br />
             and second.</h1>
            </div>
-                <p className='lg:text-xl mt-4 lg:mt-2 text-sm font-bold text-white'>what’s yours remains only yours.</p>
+                <p className='lg:text-xl mt-4 lg:mt-2 md:text-xl text-sm font-bold text-white'>what’s yours remains only yours.</p>
                 <div className='w-[80vw] mt-10 lg:mt-0'>
-                <p className='text-white  mt-16 text-[14px] font-semibold'>CRED ensures that all your personal data and transactions are encrypted, <br /> and secured so what’s yours remains only yours. there’s no room for <br /> mistakes because we didn’t leave any.</p>
+                <p className='text-white md:text-xl mt-16 text-[14px] font-semibold'>CRED ensures that all your personal data and transactions are encrypted, <br className='lg:flex hidden'/> and secured so what’s yours remains only yours. there’s no room for <br  className='lg:flex hidden'/> mistakes because we didn’t leave any.</p>
                 </div>
-                <button className='lg:px-10 px-8 hover:bg-transparent border-2 hover:text-white py-2 mt-8 bg-white text-black font-semibold text-base lg:text-lg rounded-full'>Become a member</button>
+                <button className='lg:px-10 px-8 hover:bg-transparent border-2 hover:text-white py-2 mt-8 bg-white text-black font-semibold text-base lg:py-2 md:py-3 lg:text-lg rounded-full'>Become a member</button>
         </div>
     </>
   )
